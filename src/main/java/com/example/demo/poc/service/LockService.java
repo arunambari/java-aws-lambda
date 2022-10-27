05a6572f01fb2d4fbe8b3eb3d4fef29012ddca01
arunambari <aa>
2022-10-27 15:16:22 -0500

    version 1.2 with lock

--- src/main/java/com/example/demo/poc/service/LockService.java
+++ src/main/java/com/example/demo/poc/service/LockService.java
@@ -3,7 +3,8 @@ package com.example.demo.poc.service;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBSaveExpression;
+import com.amazonaws.services.dynamodbv2.model.*;
 import com.example.demo.poc.event.TimerEvent;
 import com.example.demo.poc.handler.JobHandler;
 import com.example.demo.poc.model.LockConstant;
@@ -16,6 +17,7 @@ import java.net.UnknownHostException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -79,7 +81,20 @@ public class LockService {
         lockItem.setActive(false); //release lock
         DynamoDBMapper mapper = new DynamoDBMapper(client);
         log("updating LockItem in setStatus method"+lockItem);
-        mapper.save(lockItem);
+
+        DynamoDBSaveExpression saveExpression = new DynamoDBSaveExpression();
+        Map<String, ExpectedAttributeValue> expected = new HashMap<>();
+       // expected.put("key", new ExpectedAttributeValue().withValue(new AttributeValue().withS(key)));
+        expected.put("active", new ExpectedAttributeValue().withValue(new AttributeValue().withN("0")));
+        expected.put("ownerName", new ExpectedAttributeValue(new AttributeValue().withS(ownerName)));
+        saveExpression.setExpected(expected);
+        saveExpression.setConditionalOperator(ConditionalOperator.AND);
+        try {
+            mapper.save(lockItem, saveExpression);
+        }catch (ConditionalCheckFailedException e) {
+           // e.printStackTrace();
+            log("conditional check failed for owner "+ownerName+ "  error cause :"+e.getLocalizedMessage());
+        }
         return lockItem;
 
     }
@@ -89,8 +104,13 @@ public class LockService {
         if(lockItem == null) {
             lockItem = new LockItem(key,leaseDuration,startTime,true,ownerName,System.currentTimeMillis());
         }
-        boolean ownerOfTheLock = lockItem.getOwnerName().equals(ownerName);
+;        boolean ownerOfTheLock = lockItem.getOwnerName().equals(ownerName);
+        long diffCurrentTimeWithLeaseDuration = System.currentTimeMillis() - lockItem.getLeaseDuration();
+        long leaseRenewalTime=lockItem.getLeaseRenewalTimeInMilliSeconds();
 
+
+        log("diffCurrentTimeWithLeaseDuration  seconds ::"+diffCurrentTimeWithLeaseDuration/1000+"  ::: leaseRenewalTime" +
+                leaseRenewalTime/1000+" diffleaseRenewl - diffCurrentTimeWithLeaseDuration"+(diffCurrentTimeWithLeaseDuration-leaseRenewalTime)/1000);
         if(!ownerOfTheLock) {
               if(  System.currentTimeMillis() > lockItem.getLeaseRenewalTimeInMilliSeconds()+leaseDuration) {
 
@@ -107,7 +127,21 @@ public class LockService {
         DynamoDBMapper mapper = new DynamoDBMapper(client);
         log("updating LockItem in putItem method"+lockItem);
 
-        mapper.save(lockItem);
+        DynamoDBSaveExpression saveExpression = new DynamoDBSaveExpression();
+        Map<String, ExpectedAttributeValue> expected = new HashMap<>();
+
+
+        expected.put("active", new ExpectedAttributeValue().withValue(new AttributeValue().withN("0")));
+       expected.put("ownerName", new ExpectedAttributeValue(new AttributeValue().withS(ownerName)));
+        expected.put("leaseRenewalTimeInMilliSeconds", new ExpectedAttributeValue().withValue(new AttributeValue().withN(Long.toString(diffCurrentTimeWithLeaseDuration))).withComparisonOperator(ComparisonOperator.LT));
+
+        saveExpression.setExpected(expected);
+        saveExpression.setConditionalOperator(ConditionalOperator.OR);
+      try {
+          mapper.save(lockItem, saveExpression);
+      }catch (ConditionalCheckFailedException e) {
+          log("conditional check failed for owner "+ownerName+ "  error cause :"+e.getMessage());
+      }
 
         return lockItem;
     }
@@ -152,6 +186,9 @@ public class LockService {
     private void log(String message) {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
-        context.getLogger().log(dtf.format(now) +"---"+message);
+        if(context!=null)
+           context.getLogger().log(dtf.format(now) +"---"+message);
+        else
+            System.out.println(dtf.format(now) +"---"+message);
     }
 }
